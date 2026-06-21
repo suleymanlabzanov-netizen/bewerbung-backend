f766dd6b3125777d1a359290ab21aa604d357be8
suleymanlabzanov-netizen <suleyman.labzanov@gmail.com>
2026-06-21 02:17:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 const Imap = require('imap');
 const { simpleParser } = require('mailparser');
 const nodemailer = require('nodemailer');
